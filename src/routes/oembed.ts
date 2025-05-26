3ad2ec3190ef2f7745b589b4c5f0146308b493bc
SauceyRed <saucey@saucey.red>
2025-05-27 01:57:06 +0200

    feat: move oembed route

--- src/routes/oembed.ts
+++ src/routes/oembed.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 
 export default async (fastify: FastifyInstance, options: Object) => {
-	fastify.get("/art/view/:artist/:post/oembed.json", async (request, reply) => {
+	fastify.get("/oembed", async (request, reply) => {
 		const query = request.query as any;
 		const body = JSON.stringify({
 			author_name: query.title,
