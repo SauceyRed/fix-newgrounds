d81c40409d485bc30cdb330eeeec7aa91b7abc2d
SauceyRed <saucey@saucey.red>
2025-05-27 01:58:22 +0200

    feat: update routes in main

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
 import Fastify from "fastify";
-import index from "./routes/index.js";
-import post from "./routes/art/view/[artist]/[post]/[post].js";
-import oembed from "./routes/art/view/[artist]/[post]/oembed.js";
-import status from "./routes/users/[user]/statuses/[status].js"
+import post from "./routes/art/view/[artist]/[post]/index.js";
+import oembed from "./routes/oembed.js";
 
 async function main() {
 	const fastify = Fastify({
 		logger: true
 	});
 
-	fastify.register(index);
 	fastify.register(post);
 	fastify.register(oembed);
-	fastify.register(status)
 
 	try {
-		await fastify.listen({ port: 5430 });
+		await fastify.listen({ port: 7854 });
 	} catch (error) {
 		fastify.log.error(error);
 		process.exit(1);
